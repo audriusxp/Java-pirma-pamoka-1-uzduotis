c8ac778c140f3395317ce76e2f2aea89e14fc017
audriusxp <palm@inbox.lt>
2022-06-23 08:24:05 +0300

    sukurtas projektas,negautas neruztatas-nezinau priezasciu

---
+++ src/Programa.java
@@ -0,0 +1,19 @@
+public class Perimetras{
+    public static void main(String[] args) {
+   double krastineA = 2.2;
+   double krastineB = 3.2;
+   double krastineC = 2.6;
+   double krastineD = 3.8;
+   double perimetras = staciakampioPerimetras( krastineA,krastineB,krastineC,krastineD);
+        System.out.println("perimetras = " + perimetras);
+    }
+
+    public static double staciakampioPerimetras(double a ,double b,double c ,double d )
+    {return a + b +c + d;}
+
+
+
+
+
+
+}
